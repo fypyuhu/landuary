6177edab91cf8f11c1f4a58ab5addc53a0c2b394
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-11-26 14:17:05 +0500

    fixes

--- app/Http/Controllers/ReceivingManifestController.php
+++ app/Http/Controllers/ReceivingManifestController.php
@@ -47,7 +47,7 @@ class ReceivingManifestController extends Controller
 		$customer = Customer::find($manifest->customer_id)->first();
 		if($manifest->department_from != '' && $manifest->department_to != '') {
 			$departments = CustomerDepartment::whereBetween('id', [$manifest->department_from, $manifest->department_to])->get();
-			if($departments) {
+			if(count($departments) > 0) {
 				$department_from = $departments[0]->department_name;
 				$department_to = $departments[count($departments)-1]->department_name;
 			}
@@ -55,12 +55,12 @@ class ReceivingManifestController extends Controller
 		else if($manifest->department_from != '' || $manifest->department_to != '') {
 			$departments = CustomerDepartment::where('id', '=', $manifest->department_from)->orWhere('id', '=', $manifest->department_to)->get();
 			if($manifest->department_from != '') {
-				if($departments) {
+				if(count($departments) > 0) {
 					$department_from = $departments[0]->department_name;
 					$department_to = '';
 				}
 			} else {
-				if($departments) {
+				if(count($departments) > 0) {
 					$department_to = $departments[0]->department_name;
 					$department_from = '';
 				}
