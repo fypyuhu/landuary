2feb5d3834e7c83ccfedb5f6fc4af286d9e74f6c
Hafiz Abdul Moid <moidddp@gmail.com>
2015-12-15 21:34:38 +0500

    edit tax changes

--- app/Http/Controllers/TaxController.php
+++ app/Http/Controllers/TaxController.php
@@ -90,43 +90,37 @@ class TaxController extends Controller {
     }
 
     public function getEdit($id) {
-        return view('admin.taxes.edit', ['current' => Tax::find($id)]);
+        return view('admin.taxes.edit', ['tax' => Tax::with('components')->find($id)]);
     }
 
     public function postEdit($id, EditTax $request) {
-        $records = DB::table('taxes')->where('organization_id', '=', Auth::user()->organization_id)->where('is_deleted', '<=', '0')->get();
+        $tax= Tax::find($id);
+        $tax->tax_name = $request->tax_name;
+        $tax->tax_type = $request->tax_type;
+        if ($request->tax_type == 'single') {
+            $tax->agency_name = $request->agency_name;
+            $tax->tax_rate = $request->tax_rate;
+        }
+        $tax->save();
 
-        $data = array();
-        foreach ($records as $record) {
-            $row = array();
-            $row["tax_type"] = $record->tax_type;
-            $row["tax_name"] = $record->tax_name;
-            $row["agency_name"] = $record->agency_name;
-            $row["tax_rate"] = $record->tax_rate > 0 ? $record->tax_rate : '';
-            $row["actions"] = '<a href="/admin/taxes/edit/' . $record->id . '" data-mode="ajax" >Edit</a> / <a href="/admin/taxes/delete/' . $record->id . '" data-mode="ajax">Delete</a>';
-            $data[] = $row;
+        if ($request->tax_type == 'combined') {
+            $component_name = $request->component_name;
+            $component_agency_name = $request->component_agency_name;
+            $component_tax_rate = $request->component_tax_rate;
 
-            $components = DB::table('taxes')
-                    ->join('taxes_components', 'taxes.id', '=', 'taxes_components.tax_id')
-                    ->select('taxes.*', 'taxes_components.*')
-                    ->where('taxes_components.tax_id', '=', $record->id)
-                    ->where('taxes.organization_id', '=', Auth::user()->organization_id)
-                    ->where('taxes.tax_type', 'combined')
-                    ->get();
-
-            foreach ($components as $component) {
-                $sub_row = array();
-                $sub_row["tax_type"] = '--';
-                $sub_row["tax_name"] = '--';
-                $sub_row["component_name"] = '** ' . $component->component_name;
-                $sub_row["agency_name"] = $component->agency_name;
-                $sub_row["tax_rate"] = $component->tax_rate;
-                $sub_row["actions"] = '<a href="/admin/taxes/edit/' . $record->id . '" data-mode="ajax" >Edit</a> / <a href="/admin/taxes/delete/' . $record->id . '" data-mode="ajax">Delete</a>';
-                $data[] = $sub_row;
+            foreach ($component_name as $key => $value) {
+                
+                $relation =TaxComponent::find($key);
+                if(!$relation){
+                    $relation =new TaxComponent;
+                }
+                $relation->tax_id = $tax->id;
+                $relation->component_name = $component_name[$key];
+                $relation->agency_name = $component_agency_name[$key];
+                $relation->tax_rate = $component_tax_rate[$key];
+                $relation->save();
             }
         }
-
-        echo "{\"data\":" . json_encode($data) . "}";
     }
 
     public function getDelete($id) {
