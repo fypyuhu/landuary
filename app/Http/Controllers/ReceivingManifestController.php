ed55fefad9b92241eb0bbdff5a8ca6f38f8bd285
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-12-16 19:03:40 +0500

    changes

--- app/Http/Controllers/ReceivingManifestController.php
+++ app/Http/Controllers/ReceivingManifestController.php
@@ -97,6 +97,68 @@ class ReceivingManifestController extends Controller
 			
 		if(!$items)
 			return back()->with('status', 'No Carts/Items found within the selected date.');
+			
+		return view('admin.receiving-manifest.receipt', [ 'user'=>$user, 'manifest' => $manifest, 'customer' => $customer, 'department' => $department, 'items' => $items, 'total_gross_weight' => array_sum($gross_weight), 'total_net_weight' => array_sum($net_weight), 'organization' => $organization ]);
+	}
+	
+	public function getViewReceipt($id) {
+		$manifest = ReceivingManifest::find($id);
+		$customer = Customer::find($manifest->customer_id)->first();
+		$organization = UserProfile::where('user_id', '=', Auth::user()->id)->first();
+		
+		/*$departments = '';
+		$department_from = '';
+		$department_to = '';
+		
+		if($manifest->department_from != '' && $manifest->department_to != '') {
+			$departments = CustomerDepartment::organization()->whereBetween('id', [$manifest->department_from, $manifest->department_to])->get();
+			if(count($departments) > 0) {
+				$department_from = $departments[0]->department_name;
+				$department_to = $departments[count($departments)-1]->department_name;
+			}
+		}
+		else if($manifest->department_from != '' || $manifest->department_to != '') {
+			$departments = CustomerDepartment::organization()->where('id', '=', $manifest->department_from)->orWhere('id', '=', $manifest->department_to)->get();
+			if($manifest->department_from != '') {
+				if(count($departments) > 0) {
+					$department_from = $departments[0]->department_name;
+					$department_to = '';
+				}
+			} else {
+				if(count($departments) > 0) {
+					$department_to = $departments[0]->department_name;
+					$department_from = '';
+				}
+			}
+		} else {
+			$departments = '';
+			$department_from = '';
+			$department_to = '';
+		}*/
+		$user=UserProfile::where('user_id','=',Auth::user()->id)->first();
+		$items = ReceivingManifest::getCustomerIncomingCartItems($manifest->customer_id, $manifest->date_from, $manifest->date_to, $manifest->department_id, true);
+		
+		$carts = array();
+		$gross_weight = array();
+		$net_weight = array();
+		
+		foreach($items as $key => $item) {
+			if(!in_array($item->incoming_cart_id, $carts)) {
+				$carts[] = $item->incoming_cart_id;
+				$gross_weight[] = $item->gross_weight;
+				$net_weight[] = $item->net_weight;
+			}
+		}
+		
+		foreach($carts as $cart) {
+			$cart = IncomingCart::find($cart);
+			$cart->invoiced = 1;
+			$cart->save();
+		}
+		
+		if($items)
+			$department = $items[0]->department_name;
+			
 		return view('admin.receiving-manifest.receipt', [ 'user'=>$user, 'manifest' => $manifest, 'customer' => $customer, 'department' => $department, 'items' => $items, 'total_gross_weight' => array_sum($gross_weight), 'total_net_weight' => array_sum($net_weight), 'organization' => $organization ]);
 	}
 	
