d9945e2feb9685c908ce3754af4c89b97bb3641c
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-12-17 14:20:45 +0500

    changes

--- app/Models/OutgoingCart.php
+++ app/Models/OutgoingCart.php
@@ -11,7 +11,7 @@ class OutgoingCart extends Model {
      protected $dates = ['created_at', 'updated_at', 'shipping_date'];
     public static function outCartsList($request) {
         $sql = "SELECT SUM(cici.quantity) AS number_of_items,ic.invoiced, ic.id,ic.cart_id,ic.shipping_date, cd.department_name, cici.outgoing_cart_id, c.name, c.customer_number, ic.gross_weight, ic.net_weight, ic.status, ic.is_exchange_cart FROM customers_outgoing_carts_items cici, customers c, outgoing_carts ic LEFT JOIN customers_departments cd ON ic.department_id = cd.id 
-                    WHERE ic.shipping_date BETWEEN '" . date("Y-m-d", strtotime($request->start_date)) . "' AND '" . date("Y-m-d", strtotime($request->end_date)) . "' ";
+                    WHERE ic.shipping_date BETWEEN '" . date("Y-m-d", strtotime($request->start_date)) . "' AND '" . date("Y-m-d 23:59:59", strtotime($request->end_date)) . "' ";
         if ($request->name != "-1") {
             $sql.=" AND c.id='" . $request->name . "' ";
         }
@@ -21,7 +21,7 @@ class OutgoingCart extends Model {
 
     public static function readyCartsList($request) {
         $sql = "SELECT SUM(cici.quantity) AS number_of_items, ic.id,ic.cart_id, ic.shipping_date, cd.department_name, cici.outgoing_cart_id, c.name, c.customer_number, ic.gross_weight, ic.net_weight, ic.status, ic.is_exchange_cart FROM customers_outgoing_carts_items cici, customers c, outgoing_carts ic LEFT JOIN customers_departments cd ON ic.department_id = cd.id 
-                    WHERE ic.shipping_date BETWEEN '" . date("Y-m-d", strtotime($request->start_date)) . "' AND '" . date("Y-m-d", strtotime($request->end_date)) . "' ";
+                    WHERE ic.shipping_date BETWEEN '" . date("Y-m-d", strtotime($request->start_date)) . "' AND '" . date("Y-m-d 23:59:59", strtotime($request->end_date)) . "' ";
         if ($request->name != "-1") {
             $sql.=" AND c.id='" . $request->name . "' ";
         }
