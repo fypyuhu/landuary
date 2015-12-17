35ccc71715630ac2afbac6efd7d165357d16851d
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-12-17 14:27:27 +0500

    cahnges

--- app/Models/OutgoingCart.php
+++ app/Models/OutgoingCart.php
@@ -8,7 +8,7 @@ use Auth;
 class OutgoingCart extends Model {
 
     protected $table = "outgoing_carts";
-     protected $dates = ['created_at', 'updated_at', 'shipping_date'];
+    protected $dates = ['created_at', 'updated_at', 'shipping_date'];
     public static function outCartsList($request) {
         $sql = "SELECT SUM(cici.quantity) AS number_of_items,ic.invoiced, ic.id,ic.cart_id,ic.shipping_date, cd.department_name, cici.outgoing_cart_id, c.name, c.customer_number, ic.gross_weight, ic.net_weight, ic.status, ic.is_exchange_cart FROM customers_outgoing_carts_items cici, customers c, outgoing_carts ic LEFT JOIN customers_departments cd ON ic.department_id = cd.id 
                     WHERE ic.shipping_date BETWEEN '" . date("Y-m-d", strtotime($request->start_date)) . "' AND '" . date("Y-m-d 23:59:59", strtotime($request->end_date)) . "' ";
