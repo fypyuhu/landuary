f04f96b5d9c6f02dfffb0872958e0a6ab8bccc77
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-12-22 20:26:59 +0500

    git

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -28,7 +28,7 @@ class ShipmentController extends Controller
         $active_customer=Customer::find($id);
         $customers=Customer::organization()->get();
         $departments=CustomerDepartment::where('customer_id','=',$id)->get();
-		$current_department=CustomerDepartment::where('customer_id','=',$id)->where('department_id','=',$department_id)->first();
+		$current_department=CustomerDepartment::where('customer_id','=',$id)->where('id','=',$department_id)->first();
         if($department_id==-1){
             $carts=OutgoingCart::organization()->where('customer_id','=',$id)->where('status','=','Ready')->get();
         }
