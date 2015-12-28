597ce216fd63216c0d6b112242e6f05d7dafa0a6
ubaidkhan480 <ubaidkhan.se@gmail.com>
2015-12-28 21:32:55 +0500

    changes

--- app/Http/Middleware/ManageStepsForLogin.php
+++ app/Http/Middleware/ManageStepsForLogin.php
@@ -47,11 +47,11 @@ class ManageStepsForLogin
 			$org_id = Auth::user()->organization_id;
 			$org = Organization::where('id', '=', $org_id)->first();
 			$url = Request::url();
-			if(strpos($url, 'step') === false && strpos($url, 'initial-values') === false) {
+			if(strpos($url, 'step') === false || strpos($url, 'initial-values') === false) {
 				$skipped_at = intval($org->profile_skipped_at_step);
 				$current_step = intval(substr($url, -1));
-				if( $skipped_at > 0 && $skipped_at < 5 && $skipped_at > $current_step ) {
-					//return redirect('admin/profile/step'.$org->profile_skipped_at_step);
+				if( $skipped_at > 0 && $skipped_at < 5 && $skipped_at > $current_step) {
+					return redirect('admin/profile/step'.$org->profile_skipped_at_step);
 				}
 			}
 		}
