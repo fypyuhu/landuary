bb6ba38aff3b525f9a6359577a42704d19188fec
ubaidkhan480 <ubaidkhan.se@gmail.com>
2016-01-02 12:32:54 +0500

    cha

--- resources/views/admin/invoices/receipt.blade.php
+++ resources/views/admin/invoices/receipt.blade.php
@@ -42,9 +42,9 @@
             </tr>
             @foreach($invoice_data as $key=>$data)
             <tr>
-                <td>@if($key > 0 && $data->shipping_date == $data[$key-1]->shipping_date) '...' @else @date($data->shipping_date) @endif</td>
-                <td>{{$key > 0 && $data->id == $data[$key-1]->id ? '...' : $data->id}}</td>
-                <td>{{$key > 0 && $data->cart_number == $data[$key-1]->cart_number ? '...' : $data->cart_number}}</td>
+                <td>@date($data->shipping_date)</td>
+                <td>{{$data->id}}</td>
+                <td>{{$data->cart_number}}</td>
                 <td>{{$data->item_number}}</td>
                 <td>{{$data->item_name}}</td>
                 <td>{{$data->quantity}}</td>
